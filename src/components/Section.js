2b1a3e2ecf2fa871a8e9fd608752adc9f16692de
Turner Hilliard <turner.hilliard@answerrocket.com>
2022-04-20 11:08:12 -0400

    bug fix

--- src/components/Section.js
+++ src/components/Section.js
@@ -9,7 +9,7 @@ function Section (props) {
   <h2 class="subtitle">
     Become one of the first to experience EasyStream and save money big time.
   </h2>
-  <a class="" href="https://www.youtube.com/watch?v=dQw4w9WgXcQ">
+  <a class="" href="https://easystream-17f89.web.app/">
     <button class="button is-primary is-light is-large">Join Now</button>
   </a>
   <div class="container">
